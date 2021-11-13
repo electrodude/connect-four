80b841b901821eaa7e075d82ecc52551f5f4cf84
Albert Emanuel Milani <aem@themilanis.net>
2021-11-13 00:32:55 -0500

    Use tabs instead of spaces

--- js/vars.js
+++ js/vars.js
@@ -3,15 +3,15 @@ var Game = {};
 
 // Global game config
 Game.config = {
-  startingPlayer: "black", // Choose 'black' or 'red'.
-  takenMsg: "This position is already taken. Please make another choice.",
-  drawMsg: "This game is a draw.",
-  winMsg: "The winner is: ",
-  countToWin: 4,
+	startingPlayer: "black", // Choose 'black' or 'red'.
+	takenMsg: "This position is already taken. Please make another choice.",
+	drawMsg: "This game is a draw.",
+	winMsg: "The winner is: ",
+	countToWin: 4,
 
-  // note: board dimensions are zero-indexed
-  boardLength: 6,
-  boardHeight: 5,
+	// note: board dimensions are zero-indexed
+	boardLength: 6,
+	boardHeight: 5,
 };
 
 // Global Game State
